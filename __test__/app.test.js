b2a2cbb6e6a07fcbaf65b0090a31d03684074828
= <=>
2021-05-10 20:37:48 +0100

    Final version

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,25 +1,61 @@
 const request = require('supertest');
 
-const app = require('../bin/www').default;
+const app = require('../dist/app').default;
 const dataFile = require("../dist/database/database.json");
 
-// afterEach(() =>{
-//     app.close();
-// })
-// describe('GET /fetchData', function () {
-    //     it('respond with json containing a list of all users', function (done) {
-        //         request(app).get('/fetchData')
-        //             .set('Accept', 'application/json')
-        //             .expect('Content-Type', /json/)
-        //             .expect(200, done);
-        //     });
-        // });
+
+
+
         describe('GET /fetchRecords', () => {
-            test("respond with json containing a list of all data", function (done) {
+            test("respond with json containing a list of all data", function(done){
                 request(app)
                 .get("/fetchRecords")
                 .set("Accept", "application/json")
                 .expect("Content-Type", /json/)
-                .expect(200, done);
+                .expect(200,done);
+        
+
             });
         });
+        describe('GET /fetchRecords', () => {
+            test("respond with json containing a list of all data", function(done){
+                request(app)
+                .get("/fetch")
+                .expect(404,done);
+            });
+        });
+        let data = {
+            "shape": "triangle",
+            "dimension": {
+                "a": 5,
+                "b": 5,
+                "c": 6
+            }
+        };
+        let data2 = {
+            "shape": "rectan",
+            "dimension": {
+                "a": 5,
+                "b": 5
+            }}
+            describe("POST /calculate", () => {
+            
+                test("respond with 200 OK", function(done){
+                    request(app)
+                        .post("/calculate")
+                        .send(data)
+                        .set("Accept", "application/json")
+                        .expect("Content-Type", /json/)
+                        .expect(201,done)
+                })
+                
+                test('respond with 400 bad response', function (done){
+                    request(app)
+                        .post('/calculate')
+                        .send(data2)
+                        .set('Accept', 'application/json')
+                        .expect('Content-Type', /text/)
+                        .expect("Enter a valid shape")
+                        .expect(400,done)
+                })
+            });
